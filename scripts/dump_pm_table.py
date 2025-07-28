23680fdd5e6806b7f3fe645771ac0e508c029dd9
NoSFeRaTU <master.nosferatu@gmail.com>
2025-07-28 11:38:27 +0300

    Fix dkms-install, perl scripts and file modes.
    - also restore cpu stepping and also print pmtable version on initialization.

--- scripts/dump_pm_table.py
+++ scripts/dump_pm_table.py
@@ -87,6 +87,12 @@ def getCodeName():
         "Chagall",
         "Raphael",
         "Phoenix",
+        "Strix Point",
+        "Granite Ridge",
+        "Hawk Point",
+        "Storm Peak",
+        "Krackan Point",
+        "Strix Halo",
     ]
     args = read_file_str(CN_PATH, 2)
 
